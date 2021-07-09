e55e91c8a3e17bfc64f33aa7c0341c5c1d2c5ff0
mg05111982 <mg0511105@yandex.ru>
2021-07-09 14:51:29 +0300

    config

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,11 @@
 <?php
 return [
+    // set target language to be Russian
+    'language' => 'ru-RU',
+
+    // set source language to be English
+    'sourceLanguage' => 'en-US',
+
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'gatewayUrl' => 'https://example.com',
 ];
